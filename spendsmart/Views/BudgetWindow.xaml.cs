1530911881d22ae9964b3631a1a161bbb3a4649b
QuynhAnh-31 <quynhanhanh31122005@gmail.com>
2026-07-05 13:46:07 +0000

    NS

---
+++ spendsmart/Views/BudgetWindow.xaml.cs
@@ -0,0 +1,14 @@
+using System.Windows;
+using spendsmart.Services;
+using spendsmart.ViewModels;
+
+namespace spendsmart.Views;
+
+public partial class BudgetWindow : Window
+{
+    public BudgetWindow()
+    {
+        InitializeComponent();
+        DataContext = new BudgetViewModel(ServiceFactory.CreateBudgetService(), ServiceFactory.CreateCategoryService());
+    }
+}
